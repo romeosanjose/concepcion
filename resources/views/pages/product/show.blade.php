681386d6b3d9298331ffe3b37503d0a01ba0f7c2
romeosanjose <romeosanjose@gmail.com>
2016-02-04 00:18:33 +0800

    exhcnage product and project image

--- resources/views/pages/product/show.blade.php
+++ resources/views/pages/product/show.blade.php
@@ -5,113 +5,6 @@
 <link href="{{URL::asset('assets/vendor/Bootstrap-Image-Gallery-3.1.3/css/bootstrap-image-gallery.min.css')}}" rel="stylesheet">
 <link href="{{URL::asset('assets/css/slide.css')}}" rel="stylesheet">
 
-<style>
-        
-        /* jssor slider arrow navigator skin 05 css */
-        /*
-        .jssora05l                  (normal)
-        .jssora05r                  (normal)
-        .jssora05l:hover            (normal mouseover)
-        .jssora05r:hover            (normal mouseover)
-        .jssora05l.jssora05ldn      (mousedown)
-        .jssora05r.jssora05rdn      (mousedown)
-        */
-        .jssora05l, .jssora05r {
-            display: block;
-            position: absolute;
-            /* size of arrow element */
-            width: 40px;
-            height: 40px;
-            cursor: pointer;
-            background: url('/assets/images/a17.png') no-repeat;
-            overflow: hidden;
-        }
-        .jssora05l { background-position: -10px -40px; }
-        .jssora05r { background-position: -70px -40px; }
-        .jssora05l:hover { background-position: -130px -40px; }
-        .jssora05r:hover { background-position: -190px -40px; }
-        .jssora05l.jssora05ldn { background-position: -250px -40px; }
-        .jssora05r.jssora05rdn { background-position: -310px -40px; }
-
-        /* jssor slider thumbnail navigator skin 01 css */
-        /*
-        .jssort01-99-66 .p            (normal)
-        .jssort01-99-66 .p:hover      (normal mouseover)
-        .jssort01-99-66 .p.pav        (active)
-        .jssort01-99-66 .p.pdn        (mousedown)
-        */
-        .jssort01-99-66 .p {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 99px;
-            height: 66px;
-        }
-        
-        .jssort01-99-66 .t {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-        
-        .jssort01-99-66 .w {
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            width: 100%;
-            height: 100%;
-        }
-        
-        .jssort01-99-66 .c {
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            width: 95px;
-            height: 62px;
-            border: #000 2px solid;
-            box-sizing: content-box;
-            background: url('/assets/images/t01.png') -800px -800px no-repeat;
-            _background: none;
-        }
-        
-        .jssort01-99-66 .pav .c {
-            top: 2px;
-            _top: 0px;
-            left: 2px;
-            _left: 0px;
-            width: 95px;
-            height: 62px;
-            border: #000 0px solid;
-            _border: #fff 2px solid;
-            background-position: 50% 50%;
-        }
-        
-        .jssort01-99-66 .p:hover .c {
-            top: 0px;
-            left: 0px;
-            width: 97px;
-            height: 64px;
-            border: #fff 1px solid;
-            background-position: 50% 50%;
-        }
-        
-        .jssort01-99-66 .p.pdn .c {
-            background-position: 50% 50%;
-            width: 95px;
-            height: 62px;
-            border: #000 2px solid;
-        }
-        
-        * html .jssort01-99-66 .c, * html .jssort01-99-66 .pdn .c, * html .jssort01-99-66 .pav .c {
-            /* ie quirks mode adjust */
-            width /**/: 99px;
-            height /**/: 66px;
-        }
-        
-    </style>
 
 @include('layout.slide')
 
@@ -132,8 +25,6 @@
                 <dt>Product Code:</dt>
                 <dd>{{$product->product_code}}</dd>
             </dl>    
-    </div>       
-    <div class="col-md-5">
             <dl class="dl-horizontal">
                  <dt>Product Price:</dt>
                     <dd>{{$product->price}}</dd>
@@ -146,49 +37,61 @@
                     <dt>Last Updated</dt>
                     <dd>{{$product->updated_at}}</dd>
             </dl>
-    </div> 
-    <div class="col-md-4">
-         <button  type="button" class="btn btn-primary" aria-label="Left Align" id="showLeftPush">
+   
+            <button  type="button" class="btn btn-primary" aria-label="Left Align" id="showLeftPush">
                     <span>Customize Product</span>
             </button>  
     </div>
 
     @if (count($prodFiles) > 0)
-        <div class="col-md-10 ">
-            <h2> Product Images </h2>
-            <div id="jssor_1" class="box-glow" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 960px; height: 480px; overflow: hidden; visibility: hidden; background-color: #24262e; border-radius: 15px;">
-            <!-- Loading Screen -->
-            <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-                <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-                <div style="position:absolute;display:block;background:url('/assets/images/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-            </div>
-            <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 240px; width: 720px; height: 480px; overflow: hidden;">
-                    @foreach ($prodFiles as $pf)
-                    <div data-p="150.00" style="display: none;">
-                            <img data-u="thumb" src="{{'/images/'. $pf->disk_name}}" width="100" height="100"/>
-                            <img data-u="image" src="{{'/images/'. $pf->disk_name}}" style="border-radius:15px;">                        
-                    </div>   
-                    @endforeach
-            </div>
-            <!-- Thumbnail Navigator -->
-            <div data-u="thumbnavigator" class="jssort01-99-66" style="position:absolute;left:0px;top:0px;width:240px;height:480px;" data-autocenter="2">
-                <!-- Thumbnail Item Skin Begin -->
-                <div data-u="slides" style="cursor: default;">
-                    <div data-u="prototype" class="p">
-                        <div class="w">
-                            <div data-u="thumbnailtemplate" class="t"></div>
-                        </div>
-                        <div class="c"></div>
-                    </div>
-                </div>
-                <!-- Thumbnail Item Skin End -->
-            </div>
-            <!-- Arrow Navigator -->
-            <span data-u="arrowleft" class="jssora05l" style="top:158px;left:248px;width:40px;height:40px;" data-autocenter="2"></span>
-            <span data-u="arrowright" class="jssora05r" style="top:158px;right:8px;width:40px;height:40px;" data-autocenter="2"></span>
-            <a href="http://www.jssor.com" style="display:none">Slideshow Maker</a>
-            </div>
-        </div>    
+          <div id="blueimp-gallery" class="blueimp-gallery" data-use-bootstrap-modal="false">
+                 <!-- The container for the modal slides -->
+                 <div class="slides"></div>
+                 <!-- Controls for the borderless lightbox -->
+                 <h3 class="title"></h3>
+                 <a class="prev">‹</a>
+                 <a class="next">›</a>
+                 <a class="close">×</a>
+                 <a class="play-pause"></a>
+                 <ol class="indicator"></ol>
+                 <!-- The modal dialog, which will be used to wrap the lightbox content -->
+                 <div class="modal fade">
+                     <div class="modal-dialog">
+                         <div class="modal-content">
+                             <div class="modal-header">
+                                 <button type="button" class="close" aria-hidden="true">&times;</button>
+                                 <h4 class="modal-title"></h4>
+                             </div>
+                             <div class="modal-body next"></div>
+                             <div class="modal-footer">
+                                 <button type="button" class="btn btn-default pull-left prev">
+                                     <i class="glyphicon glyphicon-chevron-left"></i>
+                                     Previous
+                                 </button>
+                                 <button type="button" class="btn btn-primary next">
+                                     Next
+                                     <i class="glyphicon glyphicon-chevron-right"></i>
+                                 </button>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div id="links" class="col-md-4">
+                 <?php $ctr = 1; ?>
+                 @foreach ($prodFiles as $pf)
+                     @if ($ctr == 1)
+                     <a href="{{'/images/'. $pf->disk_name}}"  data-gallery>
+                         <img src="{{'/images/'. $pf->disk_name}}" width="500" height="300" class="box-glow">
+                     </a>
+                     @else
+                     <a href="{{'/images/'. $pf->disk_name}}"  data-gallery>
+                         <img src="{{'/images/'. $pf->disk_name}}" width="100" height="100" class="box-glow">
+                     </a>
+                     @endif
+                     <?php $ctr++; ?>
+                 @endforeach
+             </div>
     @endif        
 
     @if (count($curMaterials) > 0 )
@@ -205,7 +108,9 @@
                         </thead>
                         @foreach ($curMaterials as $curmat)
                             <tr>
-                                <td><a href="/material/detail/{{$curmat->material_name}}">{{$curmat->material_name}}</a></td>
+                                <td>
+                                    <a href="/material/detail/{{$curmat->material_name}}">{{$curmat->material_name}}</a>
+                                </td>
                                 <td>{{$curmat->material_code}}</td>
                                 <td>{{$curmat->price}}</td>
                                 <td>{{$curmat->size}}</td>
@@ -228,67 +133,3 @@
 
 </div>
 @include('layout.footer')
-
-<script>
-        jQuery(document).ready(function ($) {
-            
-            var jssor_1_SlideshowTransitions = [
-              {$Duration:1200,$Zoom:1,$Easing:{$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad},$Opacity:2},
-              {$Duration:1000,$Zoom:11,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:1200,$Zoom:1,$Rotate:1,$During:{$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
-              {$Duration:1000,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
-              {$Duration:1200,x:0.5,$Cols:2,$Zoom:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:1200,x:4,$Cols:2,$Zoom:11,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:1200,x:0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Left:$Jease$.$Swing,$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
-              {$Duration:1000,x:-4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
-              {$Duration:1200,x:-0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Left:$Jease$.$Swing,$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
-              {$Duration:1000,x:4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
-              {$Duration:1200,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
-              {$Duration:1000,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Top:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.7}},
-              {$Duration:1200,x:-4,y:2,$Rows:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Row:28},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
-              {$Duration:1200,x:1,y:2,$Cols:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:19},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.8}}
-            ];
-            
-            var jssor_1_options = {
-              $AutoPlay: true,
-              $SlideshowOptions: {
-                $Class: $JssorSlideshowRunner$,
-                $Transitions: jssor_1_SlideshowTransitions,
-                $TransitionsOrder: 1
-              },
-              $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$
-              },
-              $ThumbnailNavigatorOptions: {
-                $Class: $JssorThumbnailNavigator$,
-                $Rows: 2,
-                $Cols: 6,
-                $SpacingX: 14,
-                $SpacingY: 12,
-                $Orientation: 2,
-                $Align: 156
-              }
-            };
-            
-            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-            
-            //responsive code begin
-            //you can remove responsive code if you don't want the slider scales while window resizing
-            function ScaleSlider() {
-                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-                if (refSize) {
-                    refSize = Math.min(refSize, 960);
-                    refSize = Math.max(refSize, 300);
-                    jssor_1_slider.$ScaleWidth(refSize);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
-            ScaleSlider();
-            $(window).bind("load", ScaleSlider);
-            $(window).bind("resize", ScaleSlider);
-            $(window).bind("orientationchange", ScaleSlider);
-            //responsive code end
-        });
-    </script>
